2997627378cef780354037fe3b156f44c1c3e379
ssg <zongsa@sina.com>
2020-04-10 20:08:01 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
-
+from django.shortcuts import rediredt
 
 def index(request):
 	return HttpResponse('index')
+
+def login(request):
+	return redirect('/index')
 
